d20ea09d2a906bb66f520d27c62c2715aba0c2bd
daniel martins <freitasdaniel39@gmail.com>
2020-09-18 19:10:55 -0300

    styling form components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import 'fontsource-roboto';
+
+import { Container, Typography } from '@material-ui/core';
 
 function App() {
   return (
-    <>
-      <h1>oi</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
       <FormularioCadastro />
-    </>
+    </Container>
   );
 }
 
